274ecf77e0f3331f079bbd107c72414b06ac9b75
Shashank Jaiswal <Shashankjaiswal2304@gmail.com>
2020-10-05 14:43:44 +0530

    react setup done

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      Hi, Facebook clone
+      <Header />
+      
+    </div>
+  );
+}
+
+export default App;
